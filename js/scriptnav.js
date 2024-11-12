660945cdc2f8935d8cca768f2881b4201f2b5414
VoCongDanh22130033 <22130033@st.hcmuaf.edu.vn>
2024-11-12 23:01:54 +0700

    DANH cap nhat sua loi css dobegai

--- js/scriptnav.js
+++ js/scriptnav.js
@@ -1,11 +1,9 @@
 //=================================cuộn lên xuống thanh menu==========================
 let lastScrollTop = 0; // Vị trí cuộn trước đó
 const navbar = document.querySelector("nav"); // Chọn thanh điều hướng
-
 window.addEventListener("scroll", function () {
   const currentScroll =
     window.pageYOffset || document.documentElement.scrollTop;
-
   if (currentScroll > lastScrollTop && currentScroll > 80) {
     // Nếu cuộn xuống và đã cuộn qua 80px
     navbar.style.top = "-80px"; // Đưa navbar lên ngoài màn hình
@@ -18,18 +16,15 @@ window.addEventListener("scroll", function () {
       navbar.style.top = "0"; // Hiện navbar
     }
   }
-
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Đảm bảo không bị âm
 });
 // đăng nhập đăng kí user ở thanh menu
 const userIcon = document.querySelector(".user-menu i");
 const dropdown = document.querySelector(".user-menu .dropdown");
-
 userIcon.addEventListener("click", function () {
   dropdown.style.display =
     dropdown.style.display === "block" ? "none" : "block";
 });
-
 // Đóng menu khi nhấp bên ngoài
 window.addEventListener("click", function (event) {
   if (!event.target.closest(".user-menu")) {

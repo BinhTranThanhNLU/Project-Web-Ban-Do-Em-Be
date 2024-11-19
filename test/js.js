35645bdaca472f98d20a0821f0213b65f4dd4eb4
VoCongDanh22130033 <22130033@st.hcmuaf.edu.vn>
2024-11-19 16:35:37 +0700

    Tai code

--- test/js.js
+++
@@ -1,6 +0,0 @@
-document
-  .querySelector(".icons .fa-search")
-  .addEventListener("click", function () {
-    const hiddenSearchBar = document.getElementById("hiddenSearchBar");
-    hiddenSearchBar.classList.toggle("active");
-  });

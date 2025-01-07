9b9e0eecb4f47fa3b715c3cb253c692bd55b2f7b
VoCongDanh22130033 <22130033@hcmuaf.edu.vn>
2025-01-07 17:57:23 +0700

    DANH 7/1/2025 cap nhat chuc nang login

--- web/src/main/java/vn/edu/hcmuaf/st/web/controller/LoginController.java
+++ web/src/main/java/vn/edu/hcmuaf/st/web/controller/LoginController.java
@@ -22,17 +22,29 @@ public class LoginController extends HttpServlet {
         rd.forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//
+//        UserService service = new UserService();
+//        User user = new User(username, password);
+//        if (service.checkLogin(username,password)) {
+//            response.sendRedirect("home");
+//        } else {
+//            response.sendRedirect("login?err=1");
+//        }
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
 
-        UserService service = new UserService();
-        User user = new User(username, password);
-        if (service.checkLogin(username,password)) {
-            response.sendRedirect("home");
-        } else {
-            response.sendRedirect("login?err=1");
-        }
+    UserService service = new UserService();
+    User user = new User(username, password);
+    if (service.checkLogin(username,password)) {
+        response.sendRedirect("home");
+    } else {
+        response.sendRedirect("login?err=1");
+    }
+
     }
 
 }

d950f222cf4827932bb940ad73fc347b3c3d4a9e
hoquocbao-1804 <22130018@st.hcmuaf.edu.vn>
2025-01-17 01:10:20 +0700

    xoá phương thức getProdetaById

--- web/src/main/java/vn/edu/hcmuaf/st/web/services/ProductService.java
+++ web/src/main/java/vn/edu/hcmuaf/st/web/services/ProductService.java
@@ -1,7 +1,11 @@
 package vn.edu.hcmuaf.st.web.services;
 
 import vn.edu.hcmuaf.st.web.dao.ProductDAO;
+import vn.edu.hcmuaf.st.web.dao.ProductVariantDAO;
+import vn.edu.hcmuaf.st.web.model.Color;
 import vn.edu.hcmuaf.st.web.model.Product;
+import vn.edu.hcmuaf.st.web.model.ProductImage;
+import vn.edu.hcmuaf.st.web.model.Size;
 
 import java.util.List;
 
@@ -40,8 +44,54 @@ public class ProductService {
     public List<Product> getAllProducts() {
         return productDAO.findAll();
     }
+    public List<Product> searchProductsByName(String name) {
+        return productDAO.searchProductsByName(name);
+    }
+
+//    public Product getProductDetailsById(int idProduct) {
+//        Product product = productDAO.findProductDetailsById(idProduct);
+//        if (product != null) {
+//            ProductVariantDAO variantDAO = new ProductVariantDAO();
+//            // Gắn hình ảnh
+//            List<ProductImage> images = variantDAO.findImagesByProductId(idProduct);
+//            product.setImages(images);
+//            // Gắn danh sách màu sắc
+//            List<Color> colors = variantDAO.findColorsByProductId(idProduct);
+//            product.setColors(colors); // Sử dụng setter mới
+//            // Gắn danh sách kích thước
+//            List<Size> sizes = variantDAO.findSizesByProductId(idProduct);
+//            product.setSizes(sizes); // Sử dụng setter mới
+//        }
+//        return product;
+//    }
+public Product getProductDetailsById(int idProduct) {
+    Product product = productDAO.findProductDetailsById(idProduct);
+    if (product != null) {
+        ProductVariantDAO variantDAO = new ProductVariantDAO();
+        // Gắn hình ảnh
+        List<ProductImage> images = variantDAO.findImagesByProductId(idProduct);
+        product.setImages(images);
+        // Gắn danh sách màu sắc
+        List<Color> colors = variantDAO.findColorsByProductId(idProduct);
+        product.setColors(colors); // Sử dụng setter mới
+        // Gắn danh sách kích thước
+        List<Size> sizes = variantDAO.findSizesByProductId(idProduct);
+        product.setSizes(sizes); // Sử dụng setter mới
+    }
+    return product;
+}
+    public List<Color> getColorsByProductId(int idProduct) {
+        ProductVariantDAO variantDAO = new ProductVariantDAO();
+        return variantDAO.findColorsByProductId(idProduct);
+    }
+    public List<Size> getSizesByProductId(int idProduct) {
+        ProductVariantDAO variantDAO = new ProductVariantDAO();
+        return variantDAO.findSizesByProductId(idProduct);
+    }
+
 
     public static void main(String[] args) {
 
     }
+
 }
